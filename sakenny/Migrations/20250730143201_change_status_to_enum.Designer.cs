b02143f49d9b10e043885d091f9ba7cc18c1d2f4
Ahmed Abdeen <ahmedwalleed15@gmail.com>
2025-07-30 17:35:31 +0300

    Rename 'status' column to 'Status' in Properties table and change its type from string to int; update PropertyPermits table to use int for 'status'.

--- sakenny/Migrations/20250730143201_change_status_to_enum.Designer.cs
+++ sakenny/Migrations/20250730143201_change_status_to_enum.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using sakenny.DAL;
 
@@ -11,9 +12,11 @@ using sakenny.DAL;
 namespace sakenny.Migrations
 {
     [DbContext(typeof(ApplicationDBContext))]
-    partial class ApplicationDBContextModelSnapshot : ModelSnapshot
+    [Migration("20250730143201_change_status_to_enum")]
+    partial class change_status_to_enum
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
