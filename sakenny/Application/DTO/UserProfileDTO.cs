6e95556b1641272dbd8ac86d894c2f8abc0f7088
Rodina ElFeky <rodina.elfeky228@gmail.com>
2025-07-31 17:42:56 +0300

    View Profile-Details Creation

---
+++ sakenny/Application/DTO/UserProfileDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace sakenny.Application.DTO
+{
+    public class UserProfileDTO
+    {
+        public string Id { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string ProfilePicUrl { get; set; }
+    }
+}
