f1078eb630b8b0fa780381cc516cea877c5187fb
Ahmed Abdeen <ahmedwalleed15@gmail.com>
2025-08-05 19:16:13 +0300

    Add GetOwnedProperties endpoint and HostedPropertyDTO
    - Implemented a new endpoint in PropertyController to retrieve properties owned by the authenticated user with pagination support.
    - Created HostedPropertyDTO to structure the data returned for owned properties.
    - Updated IPropertyService and PropertyService to include the new method for fetching user-owned properties in a paginated format.

--- sakenny/Application/Interfaces/IPropertyService.cs
+++ sakenny/Application/Interfaces/IPropertyService.cs
@@ -14,6 +14,7 @@ namespace sakenny.Application.Interfaces
         Task<PropertyDTO> UpdatePropertyAsync(int id, UpdatePropertyDTO model, string userId);
         Task<PropertyDTO> GetPropertyDetailsAsync(int id);
         Task<IEnumerable<OwnedPropertyDTO>> GetUserOwnedPropertiesAsync(string userId);
+        Task<IEnumerable<HostedPropertyDTO>> GetUserOwnedPropertiesPagedAsync(string userId, int PageNumber, int PageSize);
         Task<List<OwnedPropertyDTO>> GetTopRatedPropertiesAsync();
         Task<List<GetAllPropertiesDTO>> GetAllPropertiesAsync();
 
