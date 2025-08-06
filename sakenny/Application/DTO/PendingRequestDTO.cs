d18deab88708863dd99f300b37f5e67e2f0b4792
Nancy EL Sherbiny <103130605+NancELSherbiny@users.noreply.github.com>
2025-08-07 00:23:47 +0300

    updated DTO

--- sakenny/Application/DTO/PendingRequestDTO.cs
+++ sakenny/Application/DTO/PendingRequestDTO.cs
@@ -10,7 +10,6 @@
         public decimal Price { get; set; }
         public string Type { get; set; } = null!;
         public string Location { get; set; } = null!;
-        public decimal Price { get; set; }
         public string OwnerId { get; set; } = null!;
         public string OwnerName { get; set; } = null!;
     }
